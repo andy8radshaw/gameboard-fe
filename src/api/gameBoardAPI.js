056029966c0dd9df9a5ab6276fc88d5c64910158
andy8radshaw <andy8radshaw@gmail.com>
2021-06-28 21:47:04 +0100

    set up basic api call and logged

--- src/api/gameBoardAPI.js
+++ src/api/gameBoardAPI.js
@@ -1,5 +1,8 @@
 import axios from 'axios'
 
+const boardGameId = process.env.REACT_APP_BOARDGAME_ID
+
 export const getAllCategories = () => {
-  return axios.get('https://api.boardgameatlas.com/api/game/categories?pretty=true&client_id=jUFu1P5qDU')
+  console.log(boardGameId)
+  return axios.get(`https://api.boardgameatlas.com/api/game/categories?pretty=true&client_id=${boardGameId}`)
 }
